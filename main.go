9d516ada9a00da4ab2fef5b8938d9aabfe354575
treenAthreddY <reenath@sukshi.com>
2018-12-17 12:59:10 +0530

    Force build

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func versionHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Printf("Listening on port 8000...")
+	log.Printf("Listenng on port 8000...")
 	http.HandleFunc("/version", versionHandler)
 	http.ListenAndServe(":8000", nil)
 }
